6113f0abf71562bd1735b22837037a1ff1f5ffd7
Luiz <lufnepomuceno@gmail.com>
2026-02-04 10:55:08 -0300

    adicionando aviso de erro de conexão

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,21 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://SEU_IP:3000",
+  baseURL: "http://10.7.24.96:3000",
   timeout: 10000,
 });
+
+// interceptor de erro
+api.interceptors.response.use(
+  response => response,
+  error => {
+    // não conseguiu conectar
+    if (!error.response) {
+      return Promise.reject({
+        message: "Não foi possível conectar ao servidor. Verifique sua internet ou tente novamente."
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
